f630d4bc09d6b2ed80089a2a4b5ab7ad05daa60a
Aadil Qureshi <36612205+aadilq@users.noreply.github.com>
2025-08-15 19:59:10 -0400

    Add feature for incrementing Number of Pets

--- iOS-102-Prework/ViewController.swift
+++ iOS-102-Prework/ViewController.swift
@@ -9,9 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var morePetsSwitch: UISwitch!
+    @IBOutlet weak var morePetsStepper: UIStepper!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var schoolNameTextField: UITextField!
+    
+    @IBOutlet weak var yearSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var numberOfPetsLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+    }
+    @IBAction func stepperDidChange(_ sender: UIStepper){
+        numberOfPetsLabel.text = "\(sender.value)"
     }
 
 
